cf593915691ee52073c8e4e690d62bd45320acba
Faruk khan <fk300220@gmail.com>
2022-06-19 16:19:54 +0530

    cleanup

--- apollo247/src/components/allroutes.jsx
+++ apollo247/src/components/allroutes.jsx
@@ -1,7 +1,9 @@
 import {Route,Routes} from "react-router-dom"
 import { Cartcomo } from "./cart"
 import { Home } from "./home"
+import Login from "./login"
 import { ProductsCompo } from "./Products"
+import Register from "./Register"
 // import {  MainCompoCart, MainCompoHome } from "./maincompo"
 export const AllRoutes=()=>{
     return<>
@@ -9,6 +11,8 @@ export const AllRoutes=()=>{
         <Route path="/" element={<Home/>}></Route>
         <Route path="/cart" element={<Cartcomo/>}></Route>
         <Route path="/product" element={<ProductsCompo/>}></Route>
+        <Route path="/login" element={<Login/>}></Route>
+        <Route path="/register" element={<Register/>}></Route>
     </Routes>
     </>
 }
